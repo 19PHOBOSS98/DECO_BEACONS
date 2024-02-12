e17b4e857ab5ee63528a6d7da87f035d212e5a94
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-12 13:15:27 +0800

    rendersOutsideBoundingBox and isInRenderDistance return true

--- src/main/java/net/phoboss/decobeacons/blocks/omnibeacon/OmniBeaconBlockEntityRenderer.java
+++ src/main/java/net/phoboss/decobeacons/blocks/omnibeacon/OmniBeaconBlockEntityRenderer.java
@@ -232,16 +232,13 @@ public class OmniBeaconBlockEntityRenderer implements BlockEntityRenderer<OmniBe
                 .next();
     }
 
-    public boolean rendersOutsideBoundingBox(OmniBeaconBlockEntity beaconBlockEntity) {
+    @Override
+    public boolean rendersOutsideBoundingBox(OmniBeaconBlockEntity blockEntity) {
         return true;
     }
-    public boolean isInRenderDistance(OmniBeaconBlockEntity beaconBlockEntity, Vec3d vec3d) {
-        return Vec3d.ofCenter(beaconBlockEntity.getPos()).multiply(1.0, 0.0, 1.0).isInRange(vec3d.multiply(1.0, 0.0, 1.0), (double)this.getRenderDistance());
-    }
-
 
     @Override
-    public int getRenderDistance() {
-        return 256;
+    public boolean isInRenderDistance(OmniBeaconBlockEntity blockEntity, Vec3d pos) {
+        return true;
     }
 }
